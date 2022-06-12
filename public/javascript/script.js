d8f55b1832a1522931915fbc4b57e90147d586f7
Ndivho69 <ndibo69@gmail.com>
2022-06-12 14:20:14 +0200

    done

---
+++ public/javascript/script.js
@@ -0,0 +1,11 @@
+let myVar;
+
+function myFunction(){
+    myVar = setTimeout(showPage, 3000);
+}
+function  showPage(){
+    document.getAnimations().style.dis
+}
+
+let callButton = document.getElementById('call');
+
